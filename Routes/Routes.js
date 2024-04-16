f61d4a61cf4b7e6b898dfd66e352f1b8baeabcb9
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2024-04-16 21:12:44 +0530

    feat:userLogin userVerify

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const router=express.Router();
-const {userRegistration} = require("../Controllers/userController")
-router.route('/user/register').post(userRegistration);
-router.route('/user/login').post(userRegistration);
+const {userRegistration, userVerify, userLogin} = require("../Controllers/userController")
+const tryCatch = require("../Middlewares/tryCatch")
+router.route('/user/register').post(tryCatch(userRegistration));
+router.route('/user/login').post(tryCatch(userLogin));
+router.route('/user/:userId/verify/:token').put(tryCatch(userVerify))
 
 
 module.exports = router
